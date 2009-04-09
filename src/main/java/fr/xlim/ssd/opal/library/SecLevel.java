435909b5cafadbfe2ae30bbb3f1f397d8a82ace7
David Pequegnot <david.pequegnot@gmail.com>
2009-04-09 14:13:13 +0000

    Adding new constants for SCP02

--- src/main/java/fr/xlim/ssd/opal/library/SecLevel.java
+++ src/main/java/fr/xlim/ssd/opal/library/SecLevel.java
@@ -3,12 +3,19 @@ package fr.xlim.ssd.opal.library;
 /**
  * @author Damien Arcuset, Eric Linke
  * @author Julien Iguchi-Cartigny
+ * @author David Pequegnot, Julie Rispal
  */
 public enum SecLevel {
 
     NO_SECURITY_LEVEL((byte) 0x00),
     C_MAC((byte) 0x01),
-    C_ENC_AND_MAC((byte) 0x03);
+    C_ENC_AND_MAC((byte) 0x03),
+    
+    // Added by the OPALGUI Team but not yet implemented
+    R_MAC((byte) 0x07),
+    C_MAC_AND_R_MAC((byte) 0x0F),
+    ENC_AND_C_MAC_AND_R_MAC((byte) 0x1F);
+
     private byte value;
 
     private SecLevel(byte val) {
@@ -19,4 +26,3 @@ public enum SecLevel {
         return this.value;
     }
 }
-
