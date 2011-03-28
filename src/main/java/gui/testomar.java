09e6908cffc45684b28e61a3120bad3fe5fb974b
Omar El-Khaldi <omar.elkhaldi@gmail.com>
2011-03-28 20:51:25 +0000

    [no commit message]

---
+++ src/main/java/gui/testomar.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui;
+
+/**
+ *
+ * @author omar
+ */
+public class testomar {
+    System.out.println("test omar");
+}
