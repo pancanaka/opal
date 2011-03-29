7262f7ee0661c8a31aef7c46a66fef085488ee99
anas chanaa <anas.chanaa@gmail.com>
2011-03-29 14:24:22 +0200

    ajout des onglet dans l'onglet Data exchanges ...

--- src/main/java/gui/DataExchanges.java
+++ src/main/java/gui/DataExchanges.java
@@ -5,7 +5,9 @@
 
 package gui;
 
+import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  *
@@ -14,9 +16,38 @@ import javax.swing.JPanel;
 public class DataExchanges extends JPanel {
 
     public String title = "Data Exchanges";
+    public JTabbedPane sub_panel = new JTabbedPane();
     public DataExchanges() {
+        // setting layout
+        setLayout(new BorderLayout());
+        //
+        // tabs initialisation
+        All All= new All();
+        Apdu Apdu = new Apdu();
+        Logging Logging = new Logging();
+
+        //adding tabs to panel
+        sub_panel.addTab(All.title, All);
+        sub_panel.addTab(Apdu.title, Apdu);
+        sub_panel.addTab(Logging.title, Logging);
+        add(sub_panel,BorderLayout.CENTER);
+
+
+
+
     }
 
+    public class All extends JPanel{
+        public String title = "All";
+    }
+
+    public class Apdu extends JPanel{
+        public String title = "APDU";
+    }
+
+    public class Logging extends JPanel{
+        public String title = "Loggings";
+    }
     
 
 }
