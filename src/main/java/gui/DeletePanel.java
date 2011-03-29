c53b65c44d4c1b66c5fbc6569175ccc8496b9eaa
anas chanaa <anas.chanaa@gmail.com>
2011-03-29 12:38:08 +0200

    onglet de suppression Done ...

--- src/main/java/gui/DeletePanel.java
+++ src/main/java/gui/DeletePanel.java
@@ -1,25 +1,48 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
-import java.awt.GridLayout;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 
 /**
- *
+ * 
+ * @author chanaa
  * @author razaina
+ *
+ * the DeletePanel class serves to instantiate the deletion tab
  */
 public class DeletePanel extends JPanel{
 
     public String title = "Delete";
+    /**
+     * DeletePanel constructor
+     */
     public DeletePanel()
     {
+        setLayout(new BorderLayout());
         JPanel jplPanel = new JPanel();
-	JLabel jlbDisplay = new JLabel("Delete view");
-	jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
-	jplPanel.setLayout(new GridLayout(1, 1));
-	jplPanel.add(jlbDisplay);
-        add(jplPanel);
+        add(jplPanel,BorderLayout.WEST);
+        jplPanel.setLayout(new FlowLayout());
+
+        JLabel deletelab = new JLabel("AID");
+        jplPanel.add(deletelab);
+
+        JTextField AID = new JTextField(40);
+        jplPanel.add(AID);
+
+        JPanel boutton_panel = new JPanel();
+        boutton_panel.setLayout(new FlowLayout());
+        add(boutton_panel,BorderLayout.SOUTH);
+
+        JButton Delete = new JButton("Delete");
+        boutton_panel.add(Delete);
+        
+        
+       
     }
 }
