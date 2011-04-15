86eb462bd1bb712599d78b79f230589cc6cdcc42
Thibault <Thibault@iMac-de-Thibault.local>
2011-04-15 21:01:24 +0200

    New class ProfileComponent useful to manage profiles

---
+++ opal-gui/src/main/java/fr/xlim/ssd/opal/gui/view/components/ProfileComponent.java
@@ -0,0 +1,36 @@
+package fr.xlim.ssd.opal.gui.view.components;
+
+import java.util.ArrayList;
+
+/**
+ * Container for one profile
+ *
+ * @author Thibault Desmoulins
+ */
+public class ProfileComponent {
+    private String name, description, AID, SCPmode, TP;
+    private String[] ATR;
+    private ArrayList<KeyComponent> Keylist = new ArrayList<KeyComponent>();
+
+    public ProfileComponent(String name, String description, String AID, String SCPmode, String TP, String[] ATR) {
+        this.name = name;
+        this.description = description;
+        this.AID = AID;
+        this.SCPmode = SCPmode;
+        this.TP = TP;
+        this.ATR = ATR;
+    }
+
+    public String getName() { return this.name; }
+    public String getDescription() { return this.description; }
+    public String getAID() { return this.AID; }
+    public String getSCPmode() { return this.SCPmode; }
+    public String getTP() { return this.TP; }
+    public String[] getATR() { return this.ATR; }
+    public ArrayList getKeylist() { return this.Keylist; }
+
+    public void addKey(KeyComponent key) {
+        Keylist.add(key);
+    }
+
+}
